aad12699fcb560b1fcc129abaa63369bbeed9526
Daniel Latham <daniellatham11@gmail.com>
2023-08-11 00:19:45 -0500

    Add TcpListener and stream reader

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,29 @@
-mod server {
-    pub struct Server {
-        addr: String,
+use std::io::Read;
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
     }
 
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self { addr }
-        }
+    pub fn run(self) {
+        println!("Listening on {}", self.addr);
 
-        pub fn run(self) {
-            println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer = [0; 1024];
+                    stream.read(&mut buffer);
+                    println!("OK");
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
         }
     }
 }
