bfb5e8dc475b6f2af6143c624452f53eb859737b
Daniel Latham <daniellatham11@gmail.com>
2023-08-14 23:33:07 -0500

    Add request try_from convert skeleton

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -22,6 +25,13 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => {
+                                    println!("Failed to convert u8 slice to request: {}", e);
+                                }
+                            }
                         }
                         Err(e) => {
                             println!("Failed to read form connection: {}", e);
