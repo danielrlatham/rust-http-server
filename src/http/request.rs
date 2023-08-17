b454b39a250b51501a8bf410b976992914419edc
Daniel Latham <daniellatham11@gmail.com>
2023-08-16 22:03:37 -0500

    Add start of lifetimes

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,20 +6,20 @@ use std::str;
 use std::str::Utf8Error;
 
 pub struct Request {
-    path: String,
-    query_string: Option<String>,
+    path: &str,
+    query_string: Option<&str>,
     method: Method,
 }
 
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    // GET /search?name=abc&sort=1 HTTP/1.1
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -28,7 +28,17 @@ impl TryFrom<&[u8]> for Request {
 
         let method: Method = method.parse()?;
 
-        unimplemented!()
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
+
+        Ok(Self {
+            path: path,
+            query_string,
+            method,
+        })
     }
 }
 
