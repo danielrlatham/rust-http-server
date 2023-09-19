ea1e1a8d362c362d549d984226881bed763673f1
Daniel Latham <daniellatham11@gmail.com>
2023-09-18 21:44:27 -0500

    Add functional http response

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,6 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::convert::TryInto;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -20,17 +19,28 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((mut stream, addr)) => {
+                Ok((mut stream, _addr)) => {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {:?}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
-                                Ok(request) => {}
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                    dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1> it works</h1>".to_string()),
+                                    )
+                                }
                                 Err(e) => {
                                     println!("Failed to convert u8 slice to request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
                                 }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => {
